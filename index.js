b60ead0eaff17d39429e4e54f6a0a43009e877ac
Buse Şentürk <buse.senturk@outlook.com>
2021-06-15 10:55:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 class Tree {
   constructor(spoecies){this.species = species}
-}
+
 static definition() {
   return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+}
 }
 
 // Write your classes here
