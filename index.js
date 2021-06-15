60dbbd40bf568dd6b5fc0bd1fb58f1034311812c
Buse Şentürk <buse.senturk@outlook.com>
2021-06-15 10:56:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ static definition() {
 }
 }
 
+class Deciduous extends Tree {
+  constructor( species, name ) {
+    super( species )
+    this.name = name
+  }
+
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
+  }
+}
+
+class Evergreen extends Tree {
+  constructor( species, name ) {
+    super( species )
+    this.name = name
+  }
+
+  static definition() {
+    return `${super.definition()} Evergreens keep their leaves all year round.`
+  }
+}
 // Write your classes here
